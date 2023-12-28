a5ea591d54e909d0a8eb9d3b2ae7150f471d8d78
orhun Erdogan <orhun2303@gmail.com>
2023-12-28 13:34:46 +0100

    forgot to take away the non unique numbers

---
+++ Multiplesf3Or5/solution.py
@@ -0,0 +1,22 @@
+def getMul1(num , aimedNumber):
+    i = 0
+    mList =[]
+    while num*i < aimedNumber:
+        mList.append(num*i)
+        i += 1
+    return mList
+ 
+def getMul2(num , aimedNumber):
+    j = 0
+    zList =[]
+    while num*j < aimedNumber:
+        zList.append(num*j)
+        j += 1
+    return zList
+
+result = getMul1(3, 1000)
+result1 = getMul2(5 ,1000)
+w = set(result).union(result1) #important step 
+t = sum(w)
+
+print("sum is ", t)
