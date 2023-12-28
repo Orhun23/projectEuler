8be279c7b2faff836915d17315e77526132a4b27
orhun Erdogan <orhun2303@gmail.com>
2023-12-28 16:22:41 +0100

    pay attention on the 4 mil

---
+++ fibonaccimf/fibonacci.py
@@ -0,0 +1,17 @@
+nterms = int(input("how many term big mans? "))
+count = 0 
+a = 1 
+b = 2 
+z = 0
+
+if nterms > 0 : 
+    while count < nterms and a <4000000:
+        if a%2 == 0:
+            z += a 
+            #print(a)     
+        ntw = a + b 
+        a = b 
+        b = ntw
+        count = count + 1
+
+print("sum is", z)
