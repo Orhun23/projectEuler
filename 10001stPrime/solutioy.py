85418c152f6326985499ac1a782f9c0392e12570
orhun Erdogan <orhun2303@gmail.com>
2023-12-29 16:41:46 +0100

    less efficient

---
+++ 10001stPrime/solutioy.py
@@ -0,0 +1,15 @@
+def isPrime(number):
+    if number< 2:
+        return False
+    for i in range(2, int(number**0.5)+1):
+        if number%i == 0:
+            return False
+    return True, number
+
+num = 1 
+k=0
+while k <=10001 : 
+    if isPrime(num) is True:
+        k+=1
+    print(isPrime(num))
+    num+=1
